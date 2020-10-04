3fb35659f2b621668c7eccc6175a5cdcd42b26e1
Egor Logutov <39864474+LogutovEgor@users.noreply.github.com>
2020-10-04 23:06:20 +0300

    Подключение EntityFramework

---
+++ WebApplication/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
