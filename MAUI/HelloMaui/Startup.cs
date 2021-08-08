ee9beefacf788e025b56c6a6eb2848ae9838e034
Yehor Lohutov <39864474+LogutovEgor@users.noreply.github.com>
2021-08-08 17:36:05 +0300

    MAUI Admin Client

---
+++ MAUI/HelloMaui/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace MAUI
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IAppHostBuilder appBuilder)
+		{
+            _ = appBuilder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+		}
+	}
+}
